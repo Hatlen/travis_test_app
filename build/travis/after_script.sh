c6b122ae2a00da03ce2c7caac458cd1d4cff3eae
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 15:46:31 +0100

    trying this shit with merging to integration branch on build successs once again

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -2,5 +2,6 @@
 echo "Running after script";
 #git checkout origin/integration;
 #git merge master --no-ff --log;
-git tag -a "v0.1.2" -m "added a tag on the travis-ci machine"
-yes | git push --quiet --tags git@github.com:Hatlen/travis_test_app.git integration;
+git pull origin integration
+git checkout integration
+git merge master --quiet --no-ff --log
