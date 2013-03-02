347b2cc3bde4aa077dedb434e5ab9e53496e1924
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 16:06:10 +0100

    Changed the after_script a little bit to see if travis could do a little merge

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -6,5 +6,7 @@ git config --global user.email "trondfroding@gmail.com"
 git config --global user.name "Trond Fröding"
 git pull origin integration
 git checkout integration
+echo "Listing branches";
 git branch --list
+echo "Doing the merge thingy";
 git merge master --no-ff --log
