53a8dc0faf5e81052fefb2aac5aa1e9e75432fc7
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 11:54:47 +0100

    Pulling integration branch in after build

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 echo "Running after script";
+git pull origin integration
 git checkout integration;
 git merge master --no-ff --log;
-git push git@github.com:Hatlen/travis_test_app.git integration;
+#git push git@github.com:Hatlen/travis_test_app.git integration;
 git checkout master;
