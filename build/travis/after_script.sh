9d191ab7286fe10ef100dbeded249e607244ca54
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 12:04:55 +0100

    Remove --track option, instead just checkout origin/integration

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "Running after script";
-git checkout -b integration --track origin/integration;
+git checkout origin/integration;
 git merge master --no-ff --log;
 #git push git@github.com:Hatlen/travis_test_app.git integration;
 git checkout master;
