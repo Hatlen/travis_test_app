1aefbb6fa869a7e2a4a56ce7e8412ca27ec85cfc
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 17:53:43 +0100

    trying again

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -13,4 +13,4 @@ git checkout integration;
 echo "Doing the merge thingy";
 git merge master --no-ff --log -m "The tests passed so we create a merge master into integration branch"
 echo "Trying to do a git push"
-yes | git push origin integration;
+yes | git push git@github.com:Hatlen/travis_test_app.git integration;
