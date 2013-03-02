c433e7da5fca5bc75bdf135c9b49dd8c3b61e7f7
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 17:45:20 +0100

    another try to do the merge master to integration branch

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -4,9 +4,11 @@ echo "Running after script";
 #git merge master --no-ff --log;
 git config --global user.email "trondfroding@gmail.com"
 git config --global user.name "Trond Fröding"
-git pull origin integration
-git checkout integration
+git clone git://github.com/Hatlen/travis_test_app.git ../copy;
+cd ../copy;
 echo "Listing branches";
-git branch --list
+git branch
+git checkout integration;
+
 echo "Doing the merge thingy";
 git merge master --no-ff --log -m "The tests passed so we create a merge master into integration branch"
