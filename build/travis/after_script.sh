36fc4bf5a384b16b1dc658eabab674d34045e522
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-02 13:05:51 +0100

    Testing to add tag on travis

--- build/travis/after_script.sh
+++ build/travis/after_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "Running after script";
-git checkout origin/integration;
-git merge master --no-ff --log;
-#git push git@github.com:Hatlen/travis_test_app.git integration;
-git checkout master;
+#git checkout origin/integration;
+#git merge master --no-ff --log;
+git tag -a "travis tested" -m "added a tag on the travis-ci machine"
+yes | git push --tags git@github.com:Hatlen/travis_test_app.git integration;
