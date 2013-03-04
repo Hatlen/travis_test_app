86417c00741e57f2865e1d41ccc3bb192bdab782
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-04 08:47:14 +0100

    Using after_success instead of after_script and reading the public keys to stdout

--- build/travis/after_success_script.sh
+++ build/travis/after_success_script.sh
@@ -9,8 +9,9 @@ cd ../copy;
 echo "Listing branches";
 git branch
 git checkout integration;
-
 echo "Doing the merge thingy";
 git merge master --no-ff --log -m "The tests passed so we create a merge master into integration branch"
+echo "Now it's time to show the public keys"
+for i in $(ls ~/.ssh/ | grep -e ".*\.pub"); do echo $i; cat ~/.ssh/$i; done;
 echo "Trying to do a git push"
 yes | git push git@github.com:Hatlen/travis_test_app.git integration;
