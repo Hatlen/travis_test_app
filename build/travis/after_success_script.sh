208c14c4a935aeadf4eadd49b85321927ea61bee
Trond Fröding Hatlen <trondfroding@gmail.com>
2013-03-04 08:56:04 +0100

    Trying to list files in ~/.ssh since there was no *.pub files there

--- build/travis/after_success_script.sh
+++ build/travis/after_success_script.sh
@@ -11,7 +11,9 @@ git branch
 git checkout integration;
 echo "Doing the merge thingy";
 git merge master --no-ff --log -m "The tests passed so we create a merge master into integration branch"
+echo "Listing files in ~/.ssh"
+ls ~/.ssh/
 echo "Now it's time to show the public keys"
 for i in $(ls ~/.ssh/ | grep -e ".*\.pub"); do echo $i; cat ~/.ssh/$i; done;
 echo "Trying to do a git push"
-yes | git push git@github.com:Hatlen/travis_test_app.git integration;
+#yes | git push git@github.com:Hatlen/travis_test_app.git integration;
